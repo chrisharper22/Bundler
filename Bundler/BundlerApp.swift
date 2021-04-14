3df30b6b971471f09ee9528ce2b8096f0a80b850
chrisharper22 <19harperc.alt@gmail.com>
2021-04-14 00:15:42 -0400

    Initial Commit

---
+++ Bundler/BundlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundlerApp.swift
+//  Bundler
+//
+//  Created by Chris Harper on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BundlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
